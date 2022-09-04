64c3b2ec04f80b5cbf4f355d8c9b795431391f39
bombrary <bombra108@gmail.com>
2022-09-04 14:44:50 +0900

    add experiment code

--- src/iori.py
+++ src/iori.py
@@ -1,22 +1,12 @@
-from setup import client
 from hgm_estimation import hgm
 import numpy as np
 from multiprocessing import Pool
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 from models.iori.iori_initial import v_phis_cache
-from models.iori import particle
+from models.iori import particle, ekf
 from models.iori.phi import phi0, phi1, phi2
-
-
-def pfs_phi(client, i, zs):
-    y, mu, sig = zs
-    client.execute_string(f'subst(Pf{i}, y, {y}, mu, {mu}, sig, {sig});')
-    return np.array(client.pop_cmo())
-
-
-for i in range(0, 3):
-     client.execute_string(f'Pf{i} = bload("asir-src/pf{i}-iori2020.bin");')
+from models.iori import pfaffian
 
 
 def realize(x0: float, n: int):
@@ -44,10 +34,10 @@ def create_z0(y1, mu1):
     return np.array([y0, mu0, 1.0])
 
 
-def estimate(ys):
+def estimate(mu0, sig0, ys):
     # y = z0[0]
-    mu = 0.01
-    sig = 1
+    mu = mu0
+    sig = sig0
 
     mus = []
     sigs = []
@@ -60,9 +50,9 @@ def estimate(ys):
         # tqdm.write(f'{z0} -> {z1}')
 
         v_phi00, v_phi10, v_phi20 = v_phis_cache(*z0)
-        r0 = hgm.solve(z0, z1, v_phi00, lambda zs: pfs_phi(client, 0, zs))
-        r1 = hgm.solve(z0, z1, v_phi10, lambda zs: pfs_phi(client, 1, zs))
-        r2 = hgm.solve(z0, z1, v_phi20, lambda zs: pfs_phi(client, 2, zs))
+        r0 = hgm.solve(z0, z1, v_phi00, lambda zs: pfaffian.phi0(zs))
+        r1 = hgm.solve(z0, z1, v_phi10, lambda zs: pfaffian.phi1(zs))
+        r2 = hgm.solve(z0, z1, v_phi20, lambda zs: pfaffian.phi2(zs))
 
         v_phi0 = r0.y[:, -1]
         v_phi1 = r1.y[:, -1]
@@ -112,28 +102,61 @@ def estimate_naive(mu0, sig0, ys):
     return mus, sigs
 
 
-
-
+x0 = 10
+xs, ys = realize(x0, 100)
 y0 = 0.01
-mu0 = 0.01
+mu0 = 10.0
 sig0 = 1.0
-# v_phi00, v_phi10, v_phi20 = v_phis(y0, mu0, sig0)
-# v_phi00, v_phi10, v_phi20 = v_phis_cache(y0, mu0, sig0)
 
-xs, ys = realize(10, 100)
-result = estimate(ys)
-
+mus, sigs = estimate(mu0, sig0, ys)
 mus_naive, sigs_naive = estimate_naive(mu0, sig0, ys)
-mus_particle, sigs_particle = particle.estimate(ys, np.random.normal(loc=mu0, scale=1.0, size=100))
+mus_particle, sigs_particle = particle.estimate(ys, np.random.normal(loc=mu0, scale=np.sqrt(sig0), size=100))
+mus_ekf, sigs_ekf = ekf.estimate(mu0, sig0, ys, k=4/5, var_st=1, var_ob=1)
 
-client.send_shutdown()
 
-fig = plt.figure()
-ax = fig.add_subplot()
-ax.plot(range(len(xs)), xs)
-ax.plot(range(len(result[0])), result[0], label="HGM")
-ax.plot(range(len(mus_naive)), mus_naive, label="naive")
-ax.plot(range(len(mus_particle)), mus_particle, label="particle")
+fig: plt.Figure  = plt.figure()
+ax: plt.Axes = fig.add_subplot()
+
+
+def plot_state(ax, xs, mus, mus_naive, mus_particle, mus_ekf):
+    N = len(mus)
+    ax.set_xlabel("time")
+    ax.set_ylabel("value")
+    ax.set_title("estimation result")
+    ax.plot(range(N), xs          , label="realized state" )
+    ax.plot(range(N), mus         , label="HGM")
+    ax.plot(range(N), mus_naive   , label="naive")
+    ax.plot(range(N), mus_particle, label="particle")
+    ax.plot(range(N), mus_ekf     , label="EKF")
+
+
+def scatter_diff(ax, xs, mus, mus_naive, mus_particle, mus_ekf):
+    xs = np.array(xs, dtype=np.float64)
+    mus = np.array(mus, dtype=np.float64)
+    mus_naive = np.array(mus_naive, dtype=np.float64)
+    mus_particle = np.array(mus_particle, dtype=np.float64)
+    mus_ekf = np.array(mus_ekf, dtype=np.float64)
+
+    ds = np.abs(mus - xs)
+    ds_naive = np.abs(mus_naive - xs)
+    ds_particle = np.abs(mus_particle - xs)
+    ds_ekf = np.abs(mus_ekf - xs)
+
+    ax.set_ylabel(r"$|\mathrm{estimate} - \mathrm{state}|$")
+
+    ax.boxplot([ds, ds_naive, ds_particle, ds_ekf],
+               vert=True,
+               labels=['HGM', 'naive', 'particle', 'EKF'])
+    ax.set_title("precision of estimated state")
+
+
+plot_state(ax,[x0, *xs]
+             ,[mu0, *mus         ]
+             ,[mu0, *mus_naive   ]
+             ,[mu0, *mus_particle]
+             ,[mu0, *mus_ekf     ])
+
+# scatter_diff(ax, xs, mus, mus_naive, mus_particle, mus_ekf)
+
 ax.legend()
-fig.show()
 plt.show()
