defa4ace1da29a80559e010da66edaeaa243448a
bombrary <bombra108@gmail.com>
2022-10-20 14:05:45 +0900

    modified experiment code

--- src/iori.py
+++ src/iori.py
@@ -10,9 +10,10 @@ y0 = 0.01
 mu0 = 10.0
 sig0 = 1.0
 
-mus, sigs = hgm.estimate(mu0, sig0, ys)
-# mus_naive, sigs_naive = naive.estimate(mu0, sig0, ys)
-mus_particle, sigs_particle = particle.estimate(ys, np.random.normal(loc=mu0, scale=np.sqrt(sig0), size=100))
+mus, sigs = hgm.estimate(mu0, sig0, ys, log=True, atol=1e-10, rtol=1e-6)
+# mus_naive, sigs_naive = naive.estimate(mu0, sig0, ys) mus_particle, sigs_particle = particle.estimate(ys, np.random.normal(loc=mu0, scale=np.sqrt(sig0), size=100))
+xxs = np.random.normal(loc=mu0, scale=np.sqrt(sig0), size=70)
+mus_particle, sigs_particle = particle.estimate(ys, xxs)
 mus_ekf, sigs_ekf = ekf.estimate(mu0, sig0, ys, k=4/5, var_st=1, var_ob=1)
 mus_ukf, sigs_ukf = ukf.estimate(mu0, sig0, ys, 0.5)
 
@@ -34,7 +35,7 @@ def plot_state(ax, xs, mus, mus_particle, mus_ekf, mus_ukf):
     ax.plot(range(N), mus_ukf     , label="UKF")
     ax.legend()
 
-def scatter_diff(ax, xs, mus, mus_particle, mus_ekf, mus_ukf):
+def boxplot_diff(ax, xs, mus, mus_particle, mus_ekf, mus_ukf):
     xs = np.array(xs, dtype=np.float64)
     mus = np.array(mus, dtype=np.float64)
     # mus_naive = np.array(mus_naive, dtype=np.float64)
@@ -50,10 +51,26 @@ def scatter_diff(ax, xs, mus, mus_particle, mus_ekf, mus_ukf):
 
     ax.set_ylabel(r"$|\mathrm{estimate} - \mathrm{state}|$")
 
-    ax.boxplot([ds, ds_particle, ds_ekf, ds_ukf],
+    # ax.boxplot([ds, ds_particle, ds_ekf, ds_ukf],
+    #            vert=True,
+    #            labels=['HGM', 'particle', 'EKF', 'ukf'])
+    ax.boxplot([ds, ds_particle],
                vert=True,
-               labels=['HGM', 'particle', 'EKF', 'ukf'])
+               labels=['HGM', 'particle'])
     ax.set_title("precision of estimated state")
+
+
+def scatter_diff(ax, xs, mus, mus_particle):
+    xs = np.array(xs, dtype=np.float64)
+    mus = np.array(mus, dtype=np.float64)
+    mus_particle = np.array(mus_particle, dtype=np.float64)
+
+    ds = np.abs(mus - xs)
+    ds_particle = np.abs(mus_particle - xs)
+
+    N = len(ds)
+    ax.scatter([0] * N, ds)
+    ax.scatter([1] * N, ds_particle)
 
 # plot_state(ax,[x0, *xs]
 #              ,[mu0, *mus         ]
@@ -61,6 +78,7 @@ def scatter_diff(ax, xs, mus, mus_particle, mus_ekf, mus_ukf):
 #              ,[mu0, *mus_ekf     ]
 #              ,[mu0, *mus_ukf     ])
 
-scatter_diff(ax, xs, mus, mus_particle, mus_ekf, mus_ukf)
+scatter_diff(ax, xs, mus, mus_particle)
 
 plt.show()
+
