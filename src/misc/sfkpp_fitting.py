b9de9d995d1876088cc150a82da1168d6b48d1b3
bombrary <bombra108@gmail.com>
2022-10-30 14:22:25 +0900

    modifies layout of pyplot

--- src/misc/sfkpp_fitting.py
+++ src/misc/sfkpp_fitting.py
@@ -46,39 +46,22 @@ def fitting_nth_order(xs, y, ord, intercept, alpha):
 
 
 
-def plot_mean(ax: plt.Axes, model):
-    ax.set_xlabel("x")
-    ax.set_ylabel("S(x)")
-    ax.set_title("mean data of sFKPP process")
-
-    ax.scatter(x, moments[:, 0], label="data")
-
+def plot_mean(ax: plt.Axes, model, label: str):
     coef = model.coef_
     intercept = model.intercept_
     ord = len(coef)
     plot_x = np.linspace(0, 1, 100)
     plot_y = [np.dot(make_ord_array(x, ord), coef) + intercept for x in plot_x ]
-    ax.plot(plot_x, plot_y, color='red', label='fitting result')
-
-    ax.legend()
+    ax.plot(plot_x, plot_y, label=label)
 
 
-def plot_var(ax: plt.Axes, model, title: str):
-    ax.set_xlabel("x")
-    ax.set_ylabel("T(x)")
-    ax.set_title("var data of sFKPP process")
-
-    ax.scatter(x, moments[:, 1] - np.square(moments[:, 0]), label="data")
-
+def plot_var(ax: plt.Axes, model, label: str):
     coef = model.coef_
     intercept = model.intercept_
     ord = len(coef)
     plot_x = np.linspace(0, 1, 100)
     plot_y = [np.dot(make_ord_array(x, ord), coef) + intercept for x in plot_x ]
-    ax.plot(plot_x, plot_y, color='red', label='fitting result')
-
-    ax.set_title(title)
-    ax.legend()
+    ax.plot(plot_x, plot_y, label=label)
 
 
 N = 1000
@@ -107,24 +90,43 @@ print(f'      var(4d): {var_model_4d.coef_}')
 print(f'var(4d Lasso): {var_model_4d_lasso.coef_}')
 
 fig: plt.Figure = plt.figure()
-ax_mean0 = fig.add_subplot(2, 1, 1)
-ax_mean1 = fig.add_subplot(2, 1, 2)
+fig.subplots_adjust(left=0.05, bottom=0.07, right=0.99, top=0.95, wspace=0.24, hspace=0.23)
+ax_mean = fig.add_subplot()
 
-plot_mean(ax_mean0, mean_model_1d)
-plot_mean(ax_mean1, mean_model_2d)
+def setup_mean(ax: plt.Axes):
+    ax.set_xlabel("x")
+    ax.set_ylabel("S(x)")
+    ax.set_title("mean data of sFKPP process")
+
+    ax.scatter(x, moments[:, 0], c="lightgray", label="data")
+
+    plot_mean(ax, mean_model_1d, "1st order")
+    plot_mean(ax, mean_model_2d, "2nd order")
+
+    ax.legend()
+
+setup_mean(ax_mean)
 
 fig: plt.Figure = plt.figure()
-ax_var0 = fig.add_subplot(2, 3, 1)
-ax_var1 = fig.add_subplot(2, 3, 2)
-ax_var2 = fig.add_subplot(2, 3, 5)
-ax_var3 = fig.add_subplot(2, 3, 3)
-ax_var4 = fig.add_subplot(2, 3, 6)
+fig.subplots_adjust(left=0.05, bottom=0.07, right=0.99, top=0.95, wspace=0.24, hspace=0.23)
+ax_var = fig.add_subplot()
 
-plot_var(ax_var0, var_model_2d, "variance 2nd order")
-plot_var(ax_var1, var_model_3d, "variance 3rd order")
-plot_var(ax_var2, var_model_3d_lasso, "variance 3rd order (LASSO)")
-plot_var(ax_var3, var_model_4d, "variance 4th order")
-plot_var(ax_var4, var_model_4d_lasso, "variance 4th order (LASSO)")
+def setup_var(ax: plt.Axes):
+    ax.set_xlabel("x")
+    ax.set_ylabel("T(x)")
+    ax.set_title("var data of sFKPP process")
+    ax.scatter(x, moments[:, 1] - np.square(moments[:, 0]), c="lightgray", label="data")
+    ax.set_title("variance fitting")
+
+    plot_var(ax, var_model_2d, "2nd order")
+    plot_var(ax, var_model_3d, "3rd order")
+    plot_var(ax, var_model_3d_lasso, "3rd order (LASSO)")
+    plot_var(ax, var_model_4d, "4th order")
+    plot_var(ax, var_model_4d_lasso, "4th order (LASSO)")
+
+    ax.legend()
+
+setup_var(ax_var)
 
 plt.subplots_adjust(wspace=0.4, hspace=0.6)
 
