4c05b754a1abeb39c8f61978cbb79953249355e6
bombrary <bombra108@gmail.com>
2022-10-31 20:49:46 +0900

    modified code for ou_iori

--- src/ou_iori.py
+++ src/ou_iori.py
@@ -3,9 +3,23 @@ from .models.ou_iori import Model, realize, hgm, naive, phi, ukf, ekf, particle
 from .models.ou_iori.phi import v_phi
 from .models.ou_iori import pfaffian_gamma1on20_sigma1_varob1 as pf
 from matplotlib import pyplot as plt
+import time
 
 
-cache = {'phi0': {}, 'phi1': {}, 'phi2': {}}
+cache = {'phi0': {
+            '[0.1, 0.1, 0.01]': np.array([-0.00563109, -0.00298776, 0.02750941, 0.00460847, 0.01560604, -0.01132219, 0.29803653]),
+            '[-0.1, 0.1, 0.01]': np.array([-0.01111872, 0.01373545, 0.02538121, 0.03071762, -0.01986947, -0.00963564, 0.29447771]),
+            },
+         'phi1': {
+            '[1.0, 1.0, 0.01]': np.array([-0.02734957, -0.04698884, -0.00264924, -0.05588302, 0.31461153, 0.02847173, 0.39722788]),
+            '[-1.0, 1.0, 0.01]': np.array([0.02771979, 0.0509592, -0.00099335, 0.13752545, 0.13333263, -0.02231257, 0.04633765]),
+            '[-1.0, -1.0, 0.01]': np.array([-0.02734957, -0.04698884, 0.00264924, -0.05588302,  0.31461153, -0.02847173, -0.39722788]),
+            '[1.0, -1.0, 0.01]': np.array([0.02771979, 0.0509592, 0.00099335, 0.13752545, 0.13333263, 0.02231257, -0.04633765]),
+         },
+         'phi2': {
+             '[0.1, 0.1, 0.01]': np.array([0.00090885, 0.02222127, 0.0438181, 0.02691856, 0.08860754, 0.27355444, 0.26891495]),
+             '[-0.1, 0.1, 0.01]': np.array([0.02535506, -0.00415421, 0.04596719, 0.04082861, 0.02016327, 0.27090254, 0.26208822]),
+         }}
 def v_phis0_cache(phi, z0):
     key = str(z0)
     if key in cache[phi.__name__]:
@@ -80,6 +94,7 @@ if __name__ == '__main__':
 
     mu0 = 10.0
     sig0 = 1.0
+
     result_hgm = hgm.estimate(mu0, sig0, ys,
                               fun_z0_vphi0=lambda z1: fun_z0_vphi02(phi.phi0, z1),
                               fun_z0_vphi1=lambda z1: fun_z0_vphi1(z1),
@@ -87,7 +102,8 @@ if __name__ == '__main__':
                               pfs_phi0=pf.phi0,
                               pfs_phi1=pf.phi1,
                               pfs_phi2=pf.phi2)
-    xxs = np.random.normal(loc=mu0, scale=np.sqrt(sig0), size=500)
+
+    xxs = np.random.normal(loc=mu0, scale=np.sqrt(sig0), size=30)
     result_particle = particle.estimate(ys, xxs, model)
 
     result_naive = naive.estimate(mu0, sig0, ys, model)
