69e632190ec39fbad8605840c98c1715a51e39e3
bombrary <bombra108@gmail.com>
2022-11-03 16:26:10 +0900

    add parameter for solve_ivp's events

--- src/models/ou_iori/hgm.py
+++ src/models/ou_iori/hgm.py
@@ -6,7 +6,14 @@ def estimate(mu0, sig0, ys, *,
              fun_z0_vphi0,
              fun_z0_vphi1,
              fun_z0_vphi2,
-             pfs_phi0, pfs_phi1, pfs_phi2, log=False, disable_tqdm=False):
+             pfs_phi0,
+             pfs_phi1,
+             pfs_phi2,
+             event_phi0 = None,
+             event_phi1 = None,
+             event_phi2 = None,
+             log=False,
+             disable_tqdm=False):
     mu = mu0
     sig = sig0
 
@@ -20,11 +27,24 @@ def estimate(mu0, sig0, ys, *,
         z10, v_phi10 = fun_z0_vphi1(z1)
         z20, v_phi20 = fun_z0_vphi2(z1)
 
+        z00 = np.asarray(z00)
+        z10 = np.asarray(z10)
+        z20 = np.asarray(z20)
+
+        if event_phi0 is not None:
+            event_phi0.c = lambda t: (1-t)*z00 + t*z1
+
+        if event_phi1 is not None:
+            event_phi1.c = lambda t: (1-t)*z10 + t*z1
+
+        if event_phi2 is not None:
+            event_phi2.c = lambda t: (1-t)*z20 + t*z1
+
         if log:
-            tqdm.write(f'{z00}, {z10}, {z20} -> {z1}')
-        r0 = hgm.solve(z00, z1, v_phi00, lambda zs: pfs_phi0(zs))
-        r1 = hgm.solve(z10, z1, v_phi10, lambda zs: pfs_phi1(zs))
-        r2 = hgm.solve(z20, z1, v_phi20, lambda zs: pfs_phi2(zs))
+            tqdm.write(f'{list(z00)}, {list(z10)}, {list(z20)} -> {list(z1)}')
+        r0 = hgm.solve(z00, z1, v_phi00, lambda zs: pfs_phi0(zs), events=event_phi0)
+        r1 = hgm.solve(z10, z1, v_phi10, lambda zs: pfs_phi1(zs), events=event_phi1)
+        r2 = hgm.solve(z20, z1, v_phi20, lambda zs: pfs_phi2(zs), events=event_phi2)
 
         v_phi0 = r0.y[:, -1]
         v_phi1 = r1.y[:, -1]
