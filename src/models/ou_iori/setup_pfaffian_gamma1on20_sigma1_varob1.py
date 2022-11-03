2ed9a562ebeb55f33915912e15e38c2d921d7f65
bombrary <bombra108@gmail.com>
2022-11-03 16:26:50 +0900

    add pfaffian denominators and numerators

--- src/models/ou_iori/setup_pfaffian_gamma1on20_sigma1_varob1.py
+++ src/models/ou_iori/setup_pfaffian_gamma1on20_sigma1_varob1.py
@@ -1,7 +1,10 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 from numpy import get_include # cimport numpyを使う場合に必要
 
 # 「sample」はpyxファイルごとに修正
-ext = Extension("pfaffian_gamma1on20_sigma1_varob1", sources=["pfaffian_gamma1on20_sigma1_varob1.pyx"], include_dirs=['.', get_include()])
-setup(name="pfaffian_gamma1on20_sigma1_varob1", ext_modules=cythonize([ext], compiler_directives={'language_level':"3"}))
+ext1 = Extension("pfaffian_gamma1on20_sigma1_varob1", sources=["pfaffian_gamma1on20_sigma1_varob1.pyx"], include_dirs=['.', get_include()])
+ext2 = Extension("pfaffian_denoms_gamma1on20_sigma1_varob1", sources=["pfaffian_denoms_gamma1on20_sigma1_varob1.pyx"], include_dirs=['.', get_include()])
+ext3 = Extension("pfaffian_numer_gamma1on20_sigma1_varob1", sources=["pfaffian_numer_gamma1on20_sigma1_varob1.pyx"], include_dirs=['.', get_include()])
+setup(name="pfaffian_gamma1on20_sigma1_varob1",
+      ext_modules=cythonize([ext1, ext2, ext3], compiler_directives={'language_level':"3"}))
